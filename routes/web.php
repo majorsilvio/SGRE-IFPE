7fe03907d4c512b724b4642ccb6860015897e0bd
adrienesantana01 <adrienebarbosa22@gmail.com>
2019-05-04 18:22:21 -0300

    rotas para os edites

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/equipamento', 'EquipamentoController@index')->name('equipamento');
 Route::post('/equipamento/cadastrar','EquipamentoController@create')->name('equip_cadastro');
 Route::get('/equipamento/delete/{id}','EquipamentoController@destroy')->name('excluir_equip');
+Route::get('/equipamento/editar/{id}','EquipamentoController@edit')->name('editar_equip');
+Route::post('/equipamento/editar/{id}','EquipamentoController@update')->name('update_cadastro');
 
 Route::get('/reserva', 'ReservasController@index')->name('reserva');
-Route::post('/reservas/cadastrar','ReservasController@create')->name('reserva_cadastro');
-Route::get('/reservas/delete/{id}','ReservasController@destroy')->name('excluir_reserva');
+Route::post('/reserva/cadastrar','ReservasController@create')->name('reserva_cadastro');
+Route::get('/reserva/delete/{id}','ReservasController@destroy')->name('excluir_reserva');
+Route::get('/reserva/editar/{id}','ReservasController@edit')->name('editar_reserva');
+Route::post('/reserva/editar/{id}','ReservasController@update')->name('update_reserva');
